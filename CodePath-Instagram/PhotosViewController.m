791ce1de9886cf42b16f9ecee5d526e7f5bb5d6a
Steve Mitchell <smitchell@groupon.com>
2015-10-22 16:56:08 -0500

    hookup API call to viewDidLoad

--- CodePath-Instagram/PhotosViewController.m
+++ CodePath-Instagram/PhotosViewController.m
@@ -9,6 +9,7 @@
 #import "PhotosViewController.h"
 
 @interface PhotosViewController ()
+@property NSArray* photos;
 
 @end
 
@@ -16,7 +17,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString *urlString = @"https://api.instagram.com/v1/media/popular?client_id=6e0de7ee68c94e08a3448ccbe6c9d367";
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    
+    NSURLSession *session =
+    [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]
+                                  delegate:nil
+                             delegateQueue:[NSOperationQueue mainQueue]];
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:^(NSData * _Nullable data,
+                                                                NSURLResponse * _Nullable response,
+                                                                NSError * _Nullable error) {
+                                                if (!error) {
+                                                    NSError *jsonError = nil;
+                                                    NSDictionary *responseDictionary =
+                                                    [NSJSONSerialization JSONObjectWithData:data
+                                                                                    options:kNilOptions
+                                                                                      error:&jsonError];
+                                                    NSLog(@"Response: %@", responseDictionary[@"data"]);
+                                                    self.photos = responseDictionary[@"data"];
+                                                } else {
+                                                    NSLog(@"An error occurred: %@", error.description);
+                                                }
+                                            }];
+    [task resume];
 }
 
 - (void)didReceiveMemoryWarning {
